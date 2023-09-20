89ac73c94a83d7721824dc83a8f4a8ef32631391
Enzo Contieri <enzo.c.contieri@gmail.com>
2023-09-20 14:15:09 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Wave from './wave';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Wave size={250} progress={30}/>
       <StatusBar style="auto" />
     </View>
   );
